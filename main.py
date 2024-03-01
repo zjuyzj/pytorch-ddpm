FEATURE: New training sequence under layer-by-layer mode. Save and resume epoch info. More controllable options and better training log. FIX: end2end loss calculation.

main.py
@@ -1,7 +1,7 @@
 import copy
 import json
 import os
-import random
+# import random
 import warnings
 from absl import app, flags
 
@@ -35,13 +35,14 @@ flags.DEFINE_integer('img_size', 32, help='image size')
 flags.DEFINE_integer('warmup', 5000, help='learning rate warmup')
 flags.DEFINE_integer('batch_size', 128, help='batch size')
 flags.DEFINE_integer('num_workers', 4, help='workers of Dataloader')
-flags.DEFINE_float('ema_decay', 0.9999, help="ema decay rate")
+flags.DEFINE_float('ema_decay', 0.9999, help="ema decay rate (non-positive means disable ema)")
 flags.DEFINE_bool('parallel', False, help='multi gpu training')
 flags.DEFINE_bool('end2end', True, help='enable end-to-end training')
 flags.DEFINE_bool('cont_from_ckpt', False, help='continue training from the checkpoint')
 
 # Logging & Sampling
 flags.DEFINE_string('logdir', './ckpts', help='log directory')
+flags.DEFINE_bool('log_timestep', True, help='log the loss each timestep')
 flags.DEFINE_integer('sample_size', 64, "sampling size of images")
 flags.DEFINE_integer('sample_step', 1000, help='frequency of sampling')
 
@@ -52,22 +53,14 @@ flags.DEFINE_integer('num_images', 50000, help='the number of generated images f
 flags.DEFINE_bool('fid_use_torch', False, help='calculate IS and FID on gpu')
 flags.DEFINE_string('fid_cache', './stats/cifar10.train.npz', help='FID cache')
 
-device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+# os.environ["CUDA_VISIBLE_DEVICES"] = "2,3"
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 
-# def ema(source, target, param, decay):
 def ema(source, target, decay):
-    # with_all_param = param is None
-    # source_dict = source.state_dict(keep_vars=with_all_param)
     source_dict = source.state_dict()
     target_dict = target.state_dict()
-    # keys = [] if with_all_param else param
-    # if with_all_param:
-    #     for key in source_dict.keys():
-    #         if not source_dict[key].requires_grad:
-    #             continue
-    #         keys.append(key)
-    # for key in keys:
     for key in source_dict.keys():
         target_dict[key].data.copy_(
             target_dict[key].data * decay +
@@ -75,9 +68,11 @@ def ema(source, target, decay):
 
 
 def infiniteloop(dataloader):
+    epoch_cnt = 0
     while True:
         for x, y in iter(dataloader):
-            yield x
+            yield x, epoch_cnt
+        epoch_cnt += 1
 
 
 def warmup_lr(step):
@@ -109,7 +104,9 @@ def train():
         root='./data', train=True, download=True,
         transform=transforms.Compose([
             transforms.RandomHorizontalFlip(),
+            # Pixel range [0, 1.0]
             transforms.ToTensor(),
+            # Pixel range [-0.5, 0.5]->[-1.0, 1.0]
             transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
         ]))
     dataloader = torch.utils.data.DataLoader(
@@ -124,24 +121,16 @@ def train():
     net_model = DenoisingNet(T=FLAGS.T, cfg=model_cfg['cfg'], cfg_ratio=model_cfg['cfg_ratio'],
                              input_size=FLAGS.img_size, input_ch=3,
                              beta_1=FLAGS.beta_1, beta_T=FLAGS.beta_T).to(device)
-    ema_model = copy.deepcopy(net_model)
+    ema_model = copy.deepcopy(net_model) if FLAGS.ema_decay > 0 else None
     optim = torch.optim.Adam(net_model.parameters(), lr=FLAGS.lr)
     sched = torch.optim.lr_scheduler.LambdaLR(optim, lr_lambda=warmup_lr)
     if FLAGS.parallel:
+        if ema_model is not None:
+            ema_model = torch.nn.DataParallel(ema_model)
         net_model = torch.nn.DataParallel(net_model)
-        ema_model = torch.nn.DataParallel(ema_model)
 
-    # log setup
-    os.makedirs(os.path.join(FLAGS.logdir, 'sample'), exist_ok=True)
     # sample_noises for sampling using net_model during training process
     sample_noises = net_model.get_noise(FLAGS.sample_size, device, 'all')
-    grid = (make_grid(next(iter(dataloader))[0][:FLAGS.sample_size]) + 1) / 2
-    writer = SummaryWriter(FLAGS.logdir)
-    writer.add_image('real_sample', grid)
-    writer.flush()
-    # backup all arguments
-    with open(os.path.join(FLAGS.logdir, "flagfile.txt"), 'w') as f:
-        f.write(FLAGS.flags_into_string())
     # show model size
     model_size = 0
     for param in net_model.parameters():
@@ -149,58 +138,77 @@ def train():
         model_size += param.data.nelement()
     print('Model params: %.2f M' % (model_size / 1024 / 1024))
 
-    start_step = -1
+    start_step, base_epoch = -1, 0
     if FLAGS.cont_from_ckpt:
         ckpt = torch.load(os.path.join(FLAGS.logdir, 'ckpt.pt'))
         net_model.load_state_dict(ckpt['net_model'])
-        ema_model.load_state_dict(ckpt['ema_model'])
+        if ema_model is not None:
+            if ckpt['ema_model'] is None: ema_model = None
+            else: ema_model.load_state_dict(ckpt['ema_model'])
         sched.load_state_dict(ckpt['sched'])
         optim.load_state_dict(ckpt['optim'])
         start_step = ckpt['step']
+        base_epoch = ckpt['epoch']
         sample_noises = ckpt['sample_noises']
 
+    # log setup
+    os.makedirs(os.path.join(FLAGS.logdir, 'sample'), exist_ok=True)
+    purge_step = start_step if FLAGS.cont_from_ckpt else None
+    writer = SummaryWriter(FLAGS.logdir, purge_step=purge_step)
+    if not FLAGS.cont_from_ckpt:
+        grid = (make_grid(next(iter(dataloader))[0][:FLAGS.sample_size]) + 1) / 2
+        writer.add_image('real_sample', grid)
+        writer.flush()
+    # backup all arguments
+    with open(os.path.join(FLAGS.logdir, "flagfile.txt"), 'w') as f:
+        f.write(FLAGS.flags_into_string())
+
+    pbar_postfix = {'epoch': 0, 'loss': '0', 'lr': '0'}
+    if not FLAGS.end2end:
+        pbar_postfix.update({'layer': '0/0', 'layer_loss': '0'})
+
     # start training
-    with trange(FLAGS.total_steps, dynamic_ncols=True) as pbar:
+    with trange(start_step+1, FLAGS.total_steps, dynamic_ncols=True) as pbar:
+        # Do optimization on the full net with a mini-batch each step
         for step in pbar:
-            if step <= start_step: continue
             # train
-            optim.zero_grad()
-            x_0 = next(datalooper).to(device)
-            noise = net_model.get_noise(x_0.shape[0], device, mode='single')
-            if FLAGS.end2end:
-                x_T = net_model.add_noise(x_0, noise, FLAGS.T)
-                noises = net_model.get_noise(x_0.shape[0], device, 'all', x_T)
-                # x_0_pred, param_in_graph = (net_model(noises)+1.0)/2.0, None
-                x_0_pred = (net_model(noises)+1.0)/2.0
-                loss = F.mse_loss(x_0_pred, x_0, reduction='none')
-            else:
-                t = random.randint(2, FLAGS.T)
-                x_t = net_model.add_noise(x_0, noise, t)
-                # noise_pred, param_in_graph = net_model(x_t, t)
-                noise_pred, _ = net_model(x_t, t)
-                loss = F.mse_loss(noise_pred, noise, reduction='none')
-            mse_min, mse_max = '%.2e' % loss.min(), '%.2f' % loss.max()
-            loss = loss.mean()
-            loss.backward()
+            x_0, new_epoch = next(datalooper)
+            epoch = base_epoch+new_epoch
+            pbar_postfix['epoch'] = epoch
+            x_0 = x_0.to(device)
+            timesteps, losses = list(range(2, FLAGS.T+1)) if not FLAGS.end2end else [None], []
+            for t in timesteps: # Every layer should be optimized with the same mini-batch
+                optim.zero_grad()
+                noise = net_model.get_noise(x_0.shape[0], device, mode='single')
+                if FLAGS.end2end:
+                    x_T = net_model.add_noise(x_0, noise, FLAGS.T)
+                    noises = net_model.get_noise(x_0.shape[0], device, 'all', x_T)
+                    x_0_pred = net_model(noises)
+                    loss = F.mse_loss(x_0_pred, x_0, reduction='none').mean()
+                else:
+                    pbar_postfix['layer'] = f'{t}/{FLAGS.T}'
+                    x_t = net_model.add_noise(x_0, noise, t)
+                    noise_pred = net_model(x_t, t)
+                    loss = F.mse_loss(noise_pred, noise, reduction='none').mean()
+                    pbar_postfix['layer_loss'] = '%.2e' % loss
+                loss.backward()
+                torch.nn.utils.clip_grad_norm_(
+                    net_model.parameters(), FLAGS.grad_clip)
+                optim.step()
+                if not FLAGS.end2end and FLAGS.log_timestep:
+                    writer.add_scalar(f'loss-layer_{t}', loss, step)
+                losses.append(loss)
+                pbar.set_postfix(pbar_postfix, refresh=True)
+            avg_loss = sum(losses) / len(losses)
+            writer.add_scalar('loss', avg_loss, step)
+            pbar_postfix['loss'] = '%.2e' % avg_loss
 
-            # for p in net_model.parameters():
-            #     if p.requires_grad:
-            #         if p.grad is not None: print(p.grad)
-            #         else: cnt += 1
-            # print(cnt)
-            # exit()
+            writer.add_scalar('lr', sched.get_last_lr()[0], step)
+            pbar_postfix['lr'] = '%.2e' % sched.get_last_lr()[0]
+            sched.step() # Modify the learning rate per step
 
-            torch.nn.utils.clip_grad_norm_(
-                net_model.parameters(), FLAGS.grad_clip)
-            optim.step()
-            sched.step()
-            # ema(net_model, ema_model, param_in_graph, FLAGS.ema_decay)
-            ema(net_model, ema_model, FLAGS.ema_decay)
-
-            # log
-            if not FLAGS.end2end: writer.add_scalar(f'loss-layer_{t}', loss, step)
-            writer.add_scalar('loss', loss, step)
-            pbar.set_postfix({'loss': '%.3f' % loss, 'mse_min': mse_min, 'mse_max': mse_max})
+            if ema_model is not None:
+                ema(net_model, ema_model, FLAGS.ema_decay)
 
             # sample
             if FLAGS.sample_step > 0 and step % FLAGS.sample_step == 0:
@@ -218,10 +226,11 @@ def train():
             if FLAGS.save_step > 0 and step % FLAGS.save_step == 0:
                 ckpt = {
                     'net_model': net_model.state_dict(),
-                    'ema_model': ema_model.state_dict(),
+                    'ema_model': ema_model.state_dict() \
+                                 if ema_model is not None else None,
                     'sched': sched.state_dict(),
                     'optim': optim.state_dict(),
-                    'step': step,
+                    'step': step, 'epoch': epoch,
                     'sample_noises': sample_noises,
                 }
                 torch.save(ckpt, os.path.join(FLAGS.logdir, 'ckpt.pt'))
@@ -229,15 +238,19 @@ def train():
             # evaluate
             if FLAGS.eval_step > 0 and step % FLAGS.eval_step == 0:
                 net_IS, net_FID, _ = _eval(net_model)
-                ema_IS, ema_FID, _ = _eval(ema_model)
+                if ema_model is not None:
+                    ema_IS, ema_FID, _ = _eval(ema_model)
                 metrics = {
                     'IS': net_IS[0],
                     'IS_std': net_IS[1],
-                    'FID': net_FID,
-                    'IS_EMA': ema_IS[0],
-                    'IS_std_EMA': ema_IS[1],
-                    'FID_EMA': ema_FID
+                    'FID': net_FID
                 }
+                if ema_model is not None:
+                    metrics.update({
+                        'IS_EMA': ema_IS[0],
+                        'IS_std_EMA': ema_IS[1],
+                        'FID_EMA': ema_FID
+                    })
                 pbar.write(
                     "%d/%d " % (step, FLAGS.total_steps) +
                     ", ".join('%s:%.3f' % (k, v) for k, v in metrics.items()))
@@ -271,13 +284,14 @@ def evaluate():
         os.path.join(FLAGS.logdir, 'samples.png'),
         nrow=16)
 
-    model.load_state_dict(ckpt['ema_model'])
-    (IS, IS_std), FID, samples = _eval(model)
-    print("Model(EMA): IS:%6.3f(%.3f), FID:%7.3f" % (IS, IS_std, FID))
-    save_image(
-        torch.tensor(samples[:256]),
-        os.path.join(FLAGS.logdir, 'samples_ema.png'),
-        nrow=16)
+    if ckpt['ema_model'] is not None:
+        model.load_state_dict(ckpt['ema_model'])
+        (IS, IS_std), FID, samples = _eval(model)
+        print("Model(EMA): IS:%6.3f(%.3f), FID:%7.3f" % (IS, IS_std, FID))
+        save_image(
+            torch.tensor(samples[:256]),
+            os.path.join(FLAGS.logdir, 'samples_ema.png'),
+            nrow=16)
 
 
 def main(argv):

model.py
@@ -3,102 +3,26 @@ import torch, math
 from torch import nn
 from torch.nn import init
 from PIL import Image
-from UNet import UNet
-
-'''
-class NoisePredModule(nn.Module):
-    def __init__(self, input_size, input_ch, feat_net_cfg):
-        super().__init__()
-        # The feature extraction network is based on the shallow ResNet without bottleneck, and no stem.
-        # i.e. two 3x3 conv layers in one block with a shortcut connection, any number of block in one stage,
-        # downsample and double the number of channels only at the first layer of the first block in one stage
-        self.feat_net, multi_scale_ch = nn.ModuleList(), []
-        # The final output of the sub-network must bigger than 2x2
-        init_ch, block_in_stage = feat_net_cfg['init_ch'], feat_net_cfg['block_in_stage']
-        mid_ch, mid_size = feat_net_cfg['mid_ch'], feat_net_cfg['mid_size']
-        assert input_size >= pow(2, len(block_in_stage)+1)
-        for stage_idx, block_num in enumerate(block_in_stage):
-            is_first_stage = stage_idx == 0
-            for block_idx in range(block_num):
-                is_first_block = block_idx == 0
-                do_downsampling = is_first_block
-                if not (is_first_stage and is_first_block):
-                    in_ch = out_ch
-                    out_ch = 2*in_ch if do_downsampling else in_ch
-                else: in_ch, out_ch = input_ch, init_ch
-                multi_scale_ch.append(out_ch)
-                first_layer_stride = 2 if do_downsampling else 1
-                layers = nn.Conv2d(in_ch, out_ch, 3, first_layer_stride, 1, bias=False), \
-                         nn.BatchNorm2d(out_ch), \
-                         nn.ReLU(inplace=True), \
-                         nn.Conv2d(out_ch, out_ch, 3, 1, 1, bias=False), \
-                         nn.BatchNorm2d(out_ch)
-                self.feat_net.append(nn.Sequential(*layers))
-                shortcut = nn.Sequential(nn.Conv2d(in_ch, out_ch, 1, 2, 0, bias=False),
-                                         nn.BatchNorm2d(out_ch)) \
-                           if do_downsampling else nn.Identity()
-                self.feat_net.append(shortcut)
-                self.feat_net.append(nn.ReLU(inplace=True))
-        self.multi_scale_idx = [sum(block_in_stage[0:i+1])-1 for i in range(len(block_in_stage))]
-        multi_scale_ch = [multi_scale_ch[idx] for idx in self.multi_scale_idx]
-        self.pre_concat = nn.ModuleList()
-        # Construct multi-scale feature volume from CNN pyramid
-        for in_ch in multi_scale_ch:
-            layer = nn.Conv2d(in_ch, mid_ch, 1, bias=False), \
-                    nn.BatchNorm2d(mid_ch), \
-                    nn.ReLU(inplace=True), \
-                    nn.Upsample(mid_size, mode='bilinear', align_corners=False)
-            self.pre_concat.append(nn.Sequential(*layer))
-        # Generate the image of predicted noise from feature volume
-        pyramid_ch = len(self.pre_concat)*mid_ch
-        self.head = nn.Sequential(nn.Conv2d(pyramid_ch, pyramid_ch, 3, 1, 1, bias=True),
-                                  nn.Conv2d(pyramid_ch, input_ch, 1, 1, 0, bias=True),
-                                  nn.Upsample(input_size, mode='bilinear', align_corners=False))
-        self._initialize()
-
-    def _initialize(self):
-        for module in self.modules():
-            if isinstance(module, nn.Conv2d):
-                init.xavier_uniform_(module.weight)
-                if module.bias is not None:
-                    init.zeros_(module.bias)
-
-    def forward(self, x):
-        multi_scale_out = []
-        for block_idx in range(len(self.feat_net)//3):
-            conv, sc, act = self.feat_net[3*block_idx:3*block_idx+3]
-            x_sc = sc(x)
-            x = conv(x) + x_sc
-            x = act(x)
-            if block_idx in self.multi_scale_idx:
-                multi_scale_out.append(x)
-        for idx in range(len(multi_scale_out)):
-            seq = self.pre_concat[idx]
-            multi_scale_out[idx] = seq(multi_scale_out[idx])
-        pyramid = torch.cat(multi_scale_out, dim=1)
-        z = self.head(pyramid)
-        return z
-'''
-
+# from UNet import UNet
 
 # Simple ResNet block without downsampling and upsampling
 class NoisePredModule(nn.Module):
     def __init__(self, input_size, input_ch, feat_net_cfg):
         super().__init__()
         self.feat_net = nn.ModuleList()
-        stage_ch_fact = feat_net_cfg['stage_ch_fact']
+        stage_ch = feat_net_cfg['stage_ch']
         block_in_stage = feat_net_cfg['block_in_stage']
-        assert len(stage_ch_fact) >= 2 and stage_ch_fact[-1] == 1
-        assert len(stage_ch_fact) == len(block_in_stage)
-        for stage_idx in range(len(stage_ch_fact)):
+        assert len(stage_ch) >= 2 and stage_ch[-1] == input_ch
+        assert len(stage_ch) == len(block_in_stage)
+        for stage_idx in range(len(stage_ch)):
             for block_idx in range(block_in_stage[stage_idx]):
                 in_ch = input_ch if stage_idx == 0 and block_idx == 0 \
-                        else input_ch*stage_ch_fact[stage_idx-1] if block_idx == 0 \
-                        else input_ch*stage_ch_fact[stage_idx]
-                out_ch = input_ch*stage_ch_fact[stage_idx]
+                        else stage_ch[stage_idx-1] if block_idx == 0 \
+                        else stage_ch[stage_idx]
+                out_ch = stage_ch[stage_idx]
                 layers = nn.Conv2d(in_ch, out_ch, 3, 1, 1, bias=False), \
                         nn.BatchNorm2d(out_ch), \
-                        nn.SiLU(inplace=True), \
+                        nn.ReLU(inplace=True), \
                         nn.Conv2d(out_ch, out_ch, 3, 1, 1, bias=False), \
                         nn.BatchNorm2d(out_ch)
                 self.feat_net.append(nn.Sequential(*layers))
@@ -106,7 +30,7 @@ class NoisePredModule(nn.Module):
                         nn.Sequential(nn.Conv2d(in_ch, out_ch, 1, 1, 0, bias=False),
                                       nn.BatchNorm2d(out_ch))
                 self.feat_net.append(shortcut)
-                self.feat_net.append(nn.SiLU(inplace=True))
+                self.feat_net.append(nn.ReLU(inplace=True))
         self._initialize()
 
     def _initialize(self):
@@ -116,6 +40,8 @@ class NoisePredModule(nn.Module):
                 # module.bias is always None yet
                 if module.bias is not None:
                     init.zeros_(module.bias)
+        for block_idx in range(len(self.feat_net)//3):
+            init.xavier_uniform_(self.feat_net[3*block_idx][-2].weight, gain=1e-5)
 
     def forward(self, x):
         for block_idx in range(len(self.feat_net)//3):
@@ -130,8 +56,8 @@ class NoisePredModule(nn.Module):
 class DenoisingModule(nn.Module):
     def __init__(self, input_size, input_ch, feat_net_cfg, alpha, alpha_bar, alpha_bar_prev, large_entropy=True):
         super().__init__()
-        # self.noise_pred = NoisePredModule(input_size, input_ch, feat_net_cfg)
-        self.noise_pred = UNet(**feat_net_cfg)
+        self.noise_pred = NoisePredModule(input_size, input_ch, feat_net_cfg)
+        # self.noise_pred = UNet(**feat_net_cfg)
         # Coefficients are non-trainable, only relative to timestep T,
         # shared by all channels and all pixels, with broadcast
         self.register_buffer('coef_input', torch.sqrt(1.0/alpha))
@@ -147,6 +73,8 @@ class DenoisingModule(nn.Module):
         # Debug: Test U-Net for original DDPM
         # pred_eps = unet(x, t).detach()
         pred_eps = self.noise_pred(x)
+        # Only pred_eps is needed
+        if z is None: return pred_eps
         pred_mean = self.coef_input*x+self.coef_eps*pred_eps
         return pred_mean+self.coef_z*z
 
@@ -166,7 +94,6 @@ class DenoisingNet(nn.Module):
         # Denoise from timestep T to 1 (total T-1 layers), finally get x_1
         # To get result x_0 (timestep 0), alpha_bar_0 is needed to calculate 
         # coef_z (timestep 1) in DenoisingModule(), but it is not defined here.
-        # If needed, alpha_bar_0 = 1.0, while coef_z is the same with timestep 2.
         cfg_idx_lut = []
         assert sum(cfg_ratio) <= 1.0 and len(cfg) == len(cfg_ratio)
         for i, ratio in enumerate(cfg_ratio):
@@ -191,12 +118,9 @@ class DenoisingNet(nn.Module):
         if t is not None: # input is x_t
             assert 2 <= t <= self.T
             x = Z if len(Z.shape) == 4 else Z.squeeze()
-            noise_pred, param_in_graph = self.net[t-2].noise_pred, []
-            for k, p in noise_pred.named_parameters():
-                if not p.requires_grad: continue
-                param_in_graph.append(f'net.{t-2}.noise_pred.{k}')
-            noise = noise_pred(x)
-            return noise, param_in_graph
+            layer = self.net[t-2]
+            pred_eps = layer(x, None)
+            return pred_eps
         else:
             x = Z[:, -1, ...]
             num_layer = len(self.net)
@@ -205,7 +129,7 @@ class DenoisingNet(nn.Module):
                 z = Z[:, index, ...]
                 # Debug: Test U-Net for original DDPM
                 # timesteps = torch.ones((x.shape[0]), dtype=torch.long)
-                # timesteps = timesteps*(index+1).to(device)
+                # timesteps = (timesteps*(index+1)).to(device)
                 # x = layer(x, z, timesteps)
                 x = layer(x, z)
             return x
@@ -238,13 +162,9 @@ class DenoisingNet(nn.Module):
 if __name__ == '__main__':
     # 1 - Build the network
     device = torch.device('cpu')
-    # cfg = [{'init_ch': 32, 'block_in_stage': [3, 4, 6, 3], 'mid_ch': 16, 'mid_size': 16},
-    #        {'init_ch': 32, 'block_in_stage': [2, 2, 2, 2], 'mid_ch': 16, 'mid_size': 16},
-    #        {'init_ch': 32, 'block_in_stage': [2, 2, 2], 'mid_ch': 16, 'mid_size': 16}]
-    # cfg_ratio = [0.1, 0.3, 0.6]
-    # cfg = [{'stage_ch_fact': [1, 4, 8, 16, 64, 32, 16, 8, 4, 1],
-    #         'block_in_stage': [1, 2, 4, 4, 2, 2, 4, 4, 2, 1]}]
-    cfg = [{'ch': 32, 'ch_mult': [1, 2], 'attn': [1], 'num_res_blocks': 2, 'dropout': 0.1}]
+    # cfg = [{'ch': 32, 'ch_mult': [1, 2], 'attn': [1], 'num_res_blocks': 2, 'dropout': 0.1}]
+    cfg = [{'stage_ch': [32, 128, 128, 256, 128, 3],
+            'block_in_stage': [1, 2, 2, 2, 2, 1]}]
     cfg_ratio = [1.0]
     model = DenoisingNet(100, cfg, cfg_ratio).to(device).eval()
     # print(model)
@@ -282,7 +202,8 @@ if __name__ == '__main__':
 
     # 6 - Sample from the model
     Z = model.get_noise(5, device, 'all')
-    res = model(Z)
+    with torch.no_grad():
+        res = model(Z)
     print(res.shape)
 
     # 7 - Save the sampled image(s)

score/both.py
@@ -7,7 +7,7 @@ from .inception import InceptionV3
 from .fid import calculate_frechet_distance, torch_cov
 
 
-device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 
 def get_inception_and_fid_score(images, fid_cache, num_images=None,

score/fid.py
@@ -8,7 +8,7 @@ from .inception import InceptionV3
 
 
 DIM = 2048
-device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 
 def torch_cov(m, rowvar=False):