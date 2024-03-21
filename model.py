8296c4809a187c16bd2c33a6c80495dd22a7c0b4
Yao Zijie <1019367509@qq.com>
2024-03-21 13:57:19 +0800

    BACKPORT: support upsampling on either x_t or eps, and calculate loss either post-resizing or pre-resizing.

--- model.py
+++ model.py
@@ -1,5 +1,5 @@
 # import json
-# from collections import OrderedDict
+from collections import OrderedDict
 import torch, math
 from torch import nn
 from torch.nn.functional import interpolate
@@ -8,18 +8,19 @@ from network.unet import UNet
 from network.resnet import ResNet
 
 
-class DenoisingModule(nn.Module):
-    def __init__(self, input_size, input_ch,
-                 feat_net_type, feat_net_cfg,
-                 alpha, alpha_bar, alpha_bar_prev,
-                 large_entropy=True, use_ddim=False):
+class DenoisingModule(nn.Module): # The smallest unit for layer-by-layer training
+    def __init__(self, input_size, input_ch, feat_net_type, feat_net_cfg,
+                 alpha, alpha_bar, alpha_bar_prev, large_entropy=True, use_ddim=False,
+                 resize_fn={'func_x': None, 'func_eps': None, 'func_x_prev': None}):
         super().__init__()
+        # Mode A: 'func_x_prev' | Mode B: 'func_x'+'func_eps'
+        assert set(resize_fn.keys()) == {'func_x', 'func_eps', 'func_x_prev'}
         assert feat_net_type in ['UNet', 'ResNet']
         if feat_net_type == 'ResNet':
             self.noise_pred = ResNet(input_size, input_ch, feat_net_cfg)
         elif feat_net_type == 'UNet': 
             self.noise_pred = UNet(**feat_net_cfg, input_size=input_size, input_ch=input_ch)
-        self.use_ddim = use_ddim
+        self.use_ddim, self.resize_fn = use_ddim, resize_fn
         # Coefficients are non-trainable, only relative to timestep T,
         # shared by all channels and all pixels, with broadcast
         if not use_ddim: # Coefficient coef_z for sampling noise is needed
@@ -37,29 +38,42 @@ class DenoisingModule(nn.Module):
         self.register_buffer('coef_eps', coef_eps)
 
     def forward(self, x, z, mode='normal'):
-        assert mode in ['normal', 'with_pred_eps', 'pred_eps_only']
-        pred_eps = self.noise_pred(x)
-        if mode == 'pred_eps_only':
+        assert mode in ['normal', 'with_pred_eps', 'pred_eps_only', 'pred_eps_only_resized']
+        if mode in ['pred_eps_only', 'pred_eps_only_resized']:
             assert z is None
-            return pred_eps
-        pred_mean = self.coef_input*x+self.coef_eps*pred_eps
+        if mode == 'pred_eps_only': return self.noise_pred(x)
+        input_x = x # Backup for the original x_t
+        if self.resize_fn['func_x']:
+            x = self.resize_fn['func_x'](x)
+        pred_eps = self.noise_pred(x)
+        if self.resize_fn['func_eps']:
+            pred_eps = self.resize_fn['func_eps'](pred_eps)
+        if mode == 'pred_eps_only_resized': return pred_eps
+        pred_mean = self.coef_input*input_x+self.coef_eps*pred_eps
         # DDIM's deterministic sampling
         if self.use_ddim:
             assert z is None
             pred_x_prev = pred_mean
         else: # Normal sampling with sampling noise
-            assert z is not None
             pred_x_prev = pred_mean+self.coef_z*z
+        if self.resize_fn['func_x_prev']:
+            pred_x_prev = self.resize_fn['func_x_prev'](pred_x_prev)
         if mode == 'with_pred_eps':
             return pred_x_prev, pred_eps
         else: return pred_x_prev
 
 
 class DenoisingNet(nn.Module):
-    def __init__(self, T, net_type, cfg, ratio_cfg, ratio_size, div_factor_size,
+    def __init__(self, T, net_type, cfg, ratio_cfg,
+                 ratio_size, div_factor_size, resize_policy='x_t', loss_policy='raw', 
                  img_size=32, img_ch=3, beta_1=0.0001, beta_T=0.02, tau_S=-1):
         super().__init__()
-        self.img_ch = img_ch
+        assert resize_policy in ['x_t', 'eps']
+        assert loss_policy in ['raw', 'resized']
+        # Always calc loss on epsilon under current framework
+        if resize_policy == 'x_t': assert loss_policy == 'raw'
+        self.resize_policy, self.loss_policy, = resize_policy, loss_policy
+        self.img_size, self.img_ch = img_size, img_ch
         # Note that index 0 is corresspond to math variable of subscript 1
         betas = torch.linspace(beta_1, beta_T, T, dtype=torch.float64)
         alphas = 1.0 - betas
@@ -67,8 +81,7 @@ class DenoisingNet(nn.Module):
         # t_series from 1 to T (both end included) with interval t_step
         self.use_ddim = tau_S > 0 and tau_S <= T
         t_step = 1 if not self.use_ddim else (T-1) / (tau_S-1)
-        self.t_series = torch.arange(T, 0, -t_step).to(dtype=torch.int32)
-        self.t_series = list(self.t_series.numpy()[::-1])
+        self.t_series = list(torch.arange(1, T+1, t_step).to(dtype=torch.int32).numpy())
         assert self.t_series[0] == 1 and self.t_series[-1] == T
         t_series_idx = torch.tensor(self.t_series, dtype=torch.long)-1
         # Coefficients for forward process (add noise) 
@@ -98,56 +111,56 @@ class DenoisingNet(nn.Module):
             if idx == 0: alpha_bar_prev = torch.tensor(1.0, dtype=alpha_bar.dtype)
             else: alpha_bar_prev = alphas_bar[self.t_series[idx-1]-1]
             layer_cfg = cfg[cfg_idx_lut[t-1]]
-            last_layer_size = layer_size if idx != 0 else None
+            if resize_policy == 'x_t': # To keep the size of x_t continuous
+                last_layer_size = layer_size if idx != 0 else None
             layer_size = self.size_lut[t-1]
-            denoising = DenoisingModule(layer_size, img_ch, net_type, layer_cfg,
-                                        alpha, alpha_bar, alpha_bar_prev,
-                                        use_ddim=self.use_ddim)
-            # Upsample during denoising process (reversed t series)
-            upsample = nn.Upsample(last_layer_size, mode='bilinear', align_corners=False) \
-                       if last_layer_size and (last_layer_size != layer_size) else None
-            layer = nn.ModuleDict({'denoising': denoising, 'upsample': upsample})
+            if resize_policy == 'x_t':
+                upsample = nn.Upsample(last_layer_size, mode='bilinear', align_corners=False) \
+                           if last_layer_size and (last_layer_size != layer_size) else None
+                resize_fn={'func_x': None, 'func_eps': None, 'func_x_prev': upsample}
+            elif resize_policy == 'eps': # Denoising on img_size, and noise prediction on layer_size (input_size)
+                downsample = nn.Upsample(layer_size, mode='bilinear', align_corners=False)
+                upsample = nn.Upsample(img_size, mode='bilinear', align_corners=False)
+                resize_fn={'func_x': downsample, 'func_eps': upsample, 'func_x_prev': None}
+            layer = DenoisingModule(layer_size, img_ch, net_type, layer_cfg,
+                                    alpha, alpha_bar, alpha_bar_prev,
+                                    use_ddim=self.use_ddim, resize_fn=resize_fn)
             self.net.append(layer)
 
     # x_0 denotes pure input image
     # Shape of tensor Z is (B, len(t_series), C, H, W) for original DDPM, and None for DDIM sampling
     # If param t is not None and in range [1, T], only noise prediction of timestep t to get x_(t-1) is calculated
     def forward(self, x, Z, t='all'):
-        assert len(x.shape) == 4 # (B, C, H, W)
         if t in ['all', 'stacked']:
             num_layer = len(self.net)
-            if self.use_ddim: assert Z is None
-            else: assert isinstance(Z, list) and len(Z) == num_layer
+            if not self.use_ddim:
+                assert isinstance(Z, list) and len(Z) == num_layer
+            else: assert Z is None
             if t == 'stacked': x_all, pred_eps_all = [x], []
             for index in range(num_layer-1, -1, -1):
-                layer_size = self.size_lut[self.t_series[index]-1]
-                layer_shape = (self.img_ch, layer_size, layer_size)
-                assert x.shape[-3:] == layer_shape
-                if not self.use_ddim:
-                    z = Z[index]
-                    assert len(z.shape) == 4
-                    assert z.shape[-3:] == layer_shape
+                if not self.use_ddim: z = Z[index]
                 else: z = None
                 layer = self.net[index]
                 if t == 'stacked':
-                    x, pred_eps = layer['denoising'](x, z, mode='with_pred_eps')
+                    x, pred_eps = layer(x, z, mode='with_pred_eps')
                     x_all.insert(0, x)
                     pred_eps_all.insert(0, pred_eps)
-                    if layer['upsample']: x = layer['upsample'](x)
                 else:
-                    x = layer['denoising'](x, z, mode='normal')
-                    if layer['upsample']: x = layer['upsample'](x)
+                    x = layer(x, z, mode='normal')
             if t == 'stacked':
                 return x_all, pred_eps_all
-            return x
+            else: return x
         # Return predicted noise from timestep t to (t-1)
         elif t in self.t_series:
             assert Z is None
-            layer_size = self.size_lut[t-1]
-            layer_shape = (self.img_ch, layer_size, layer_size)
-            assert len(x.shape) == 4 and x.shape[-3:] == layer_shape
             layer = self.net[self.t_to_idx[t]]
-            pred_eps = layer['denoising'](x, None, mode='pred_eps_only')
+            # Return the original output of noise predictor to calc loss
+            if self.loss_policy == 'raw':
+                pred_eps = layer(x, None, mode='pred_eps_only')
+            # Return the resized pred_eps to calc loss, not available when 
+            # resize_mode is 'x_t' cause loss is calculated on epsilon
+            elif self.loss_policy == 'resized':
+                pred_eps = layer(x, None, mode='pred_eps_only_resized')
             return pred_eps
         return None
     
@@ -156,40 +169,48 @@ class DenoisingNet(nn.Module):
 
     # Get single noisy image x_T or noise_t from N(0, I),
     # or all sampling noises Z, or both x_T and Z
+    # noise_t is only used for training
     def get_noise(self, n, device, mode='all', t=None):
+        # x_t+raw: noise_t - layer_size, x_T and z - layer_size
+        # eps+raw: noise_t - layer_size, x_T and z - img_size
+        # eps+resized: noise_t - img_size, x_T and z - img_size
         assert mode in ['x_T', 'noise_t', 'Z', 'all']
-        if mode in ['noise_t', 'x_T', 'all']:
-            # Get x_T, t equals T
-            if mode in ['x_T', 'all']:
-                t = self.t_series[-1]
-            assert t is not None
-            layer_size_t = self.size_lut[t-1]
-            layer_shape_t = (self.img_ch, layer_size_t, layer_size_t)
-            x_t = torch.randn(n, *layer_shape_t).to(device)
-            if mode in ['noise_t', 'x_T']:
-                return x_t
-            x_T = x_t
+        if mode == 'noise_t':
+            assert t in self.t_series
+            if self.loss_policy == 'raw':
+                layer_size = self.size_lut[t-1] 
+                noise_t_shape = (self.img_ch, layer_size, layer_size)
+            elif self.loss_policy == 'resized':
+                noise_t_shape = (self.img_ch, self.img_size, self.img_size)
+            noise_t = torch.randn(n, *noise_t_shape).to(device)
+            return noise_t
+        if mode in ['x_T', 'all']:
+            x_T_size = self.img_size if self.resize_policy == 'eps' \
+                       else self.size_lut[self.t_series[-1]-1]
+            x_T_shape = (self.img_ch, x_T_size, x_T_size)
+            x_T = torch.randn(n, *x_T_shape).to(device)
+            if mode == 'x_T': return x_T
         if mode in ['Z', 'all']:
             if not self.use_ddim:
                 Z = [None] * len(self.t_series)
                 for idx, t in enumerate(self.t_series):
-                    layer_size_t = self.size_lut[t-1]
-                    layer_shape_t = (self.img_ch, layer_size_t, layer_size_t)
-                    Z[idx] = torch.randn(n, *layer_shape_t).to(device)
+                    z_size = self.img_size if self.resize_policy == 'eps' \
+                             else self.size_lut[t-1]
+                    z_shape = (self.img_ch, z_size, z_size)
+                    Z[idx] = torch.randn(n, *z_shape).to(device)
             else: Z = None
             if mode == 'Z': return Z
         return x_T, Z
     
     # Add noise_t to x_0, return x_t
+    # x_0 must be the same size with noise_t
+    # Always interpolate on x_0, never on noise_t
     def add_noise(self, x_0, noise_t, t):
         assert t in self.t_series
         t_idx = self.t_to_idx[t]
         coef_x_0 = self.coef_x_0_forward[t_idx]
         coef_noise = self.coef_noise_forward[t_idx]
-        layer_size = self.size_lut[t-1]
-        layer_shape = (self.img_ch, layer_size, layer_size)
-        x_0 = interpolate(x_0, layer_size, mode='bilinear', align_corners=False)
-        assert len(noise_t.shape) == 4 and noise_t.shape[-3:] == layer_shape
+        x_0 = interpolate(x_0, noise_t.shape[-1], mode='bilinear', align_corners=False)
         return x_0*coef_x_0+noise_t*coef_noise
     
     # ID for excluded layer start from 1
@@ -208,22 +229,16 @@ class DenoisingNet(nn.Module):
             del state_dict[key]
         # Convert the state_dict for compatibility reason, 
         # NO NEED when *.pt is generated by the same version of code
-        # new_state_dict = OrderedDict()
-        # for key in state_dict.keys():
-        #     new_key, fields = '', key.split('.')
-        #     if key == 'coef_input_forward':
-        #         new_key = 'coef_x_0_forward'
-        #     elif key == 'coef_z_forward':
-        #         new_key = 'coef_noise_forward'
-        #     elif fields[0] == 'net':
-        #         fields.insert(2, 'denoising')
-        #         for i, field in enumerate(fields):
-        #             if i == 0: new_key += field
-        #             else: new_key += f'.{field}'
-        #     else: new_key = key
-        #     new_state_dict[new_key] = state_dict[key]
-        # result = self.load_state_dict(new_state_dict, strict=False)
-        result = self.load_state_dict(state_dict, strict=False)
+        new_state_dict = OrderedDict()
+        for key in state_dict.keys():
+            if key == 'coef_input_forward':
+                new_key = 'coef_x_0_forward'
+            elif key == 'coef_z_forward':
+                new_key = 'coef_noise_forward'
+            else: new_key = key
+            new_state_dict[new_key] = state_dict[key]
+        result = self.load_state_dict(new_state_dict, strict=False)
+        # result = self.load_state_dict(state_dict, strict=False)
         assert len(result.unexpected_keys) == 0
         for missing_key in result.missing_keys:
             fields = missing_key.split('.')
@@ -245,12 +260,15 @@ if __name__ == '__main__':
     net_type, cfg = 'ResNet', [{'stage_ch': [32, 128, 128, 256, 128], 'block_in_stage': [1, 2, 2, 2, 2]},
                                {'stage_ch': [16], 'block_in_stage': [1]}]
     ratio_cfg, ratio_size, div_factor_size = [0.95, 0.05], [0.95, 0.05], [1, 2]
-    model = DenoisingNet(1000, net_type, cfg, ratio_cfg, ratio_size, div_factor_size, tau_S=20).to(device).eval()
+    resize_policy, loss_policy = 'x_t', 'raw'
+    model = DenoisingNet(1000, net_type, cfg, ratio_cfg, ratio_size, div_factor_size,
+                         resize_policy=resize_policy, loss_policy=loss_policy, tau_S=20).to(device).eval()
     # print(model)
 
     # 2 - Dump the network configurations
     # cfg_dict = {'net_type': net_type, 'cfg': cfg, 'ratio_cfg': ratio_cfg, \
-    #             'ratio_size': ratio_size, 'div_factor_size': div_factor_size}
+    #             'ratio_size': ratio_size, 'div_factor_size': div_factor_size, \
+    #             'resize_policy': resize_policy, 'loss_policy': loss_policy}
     # print(json.dumps(cfg_dict, indent=4))
 
     # 3 - Network loading and saving
@@ -299,8 +317,10 @@ if __name__ == '__main__':
     plt.savefig('samples.png')
     # plt.show() # plt.show() create a new figure
 
-    # 8 - Readd noise to certain timestep (t_s[-1])
+    # 8 - Readd noise to certain timestep (t_s[-1] a.k.a. T)
     t_s = model.get_t_series()
-    noise_t = model.get_noise(sample_size, device, mode='noise_t', t=t_s[-1])
-    x_t = model.add_noise(x_all[0], noise_t, t_s[-1])
-    print(x_t.shape)
+    noise_T = model.get_noise(sample_size, device, mode='noise_t', t=t_s[-1])
+    x_T = model.add_noise(x_all[0], noise_T, t_s[-1])
+    print(x_T.shape)
+    pred_eps = model(x_T, None, t=t_s[-1])
+    print(pred_eps.shape)
