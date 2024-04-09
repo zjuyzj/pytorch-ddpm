22edcede13d3925a7612d4cae48b6bfd2a88ba7c
Yao Zijie <1019367509@qq.com>
2024-04-09 15:08:27 +0800

    REMOVE: outdated configurations and network structures with poor performance. CHANGE: configuration which can reproduce DDPM's baseline, and an experimental layer-by-layer configuration.

---
+++ eval.sh
@@ -0,0 +1,1 @@
+CUDA_VISIBLE_DEVICES=0 python main.py --flagfile ./config/cifar10_debug.cfg --notrain --eval --fid_use_torch
---
+++ train.sh
@@ -0,0 +1,1 @@
+CUDA_VISIBLE_DEVICES=0 python main.py --flagfile ./config/cifar10_debug.cfg --train
